13bf527c24440dc914a4d170b901961853ac6d81
Jake McKenna <Jake.McKenna@Ghyston.com>
2022-05-25 14:21:51 +0100

    Add update invoice button

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
   if (msg.color) {
     console.log("Receive color = " + msg.color);
@@ -62,12 +63,42 @@ function createGetDailyRatesButton() {
   document.body.appendChild(convertToHoursButton);
 }
 
+const updateInvoice = function () {
+  const ROUNDING_TOLERANCE = 0.05;
+  // @ts-ignore
+  document.activeElement.blur();
+  Array.from(document.querySelectorAll("input.js-quantity")).forEach(i => {
+    const prevValue = i.value;
+    const newValue = i.value / 7.5;
+    const newValueRounded = (Math.round((i.value * 4) / 7.5) / 4).toFixed(2);
+    if (Math.abs(newValue - newValueRounded) > ROUNDING_TOLERANCE) {
+      alert(`Couldn't round value ${prevValue} to a quarter-day accurately (${newValue} vs ${newValueRounded}), so leaving as-is`);
+    } else {
+      i.value = (Math.round((i.value * 4) / 7.5) / 4).toFixed(2);
+    }
+  });
+  Array.from(document.querySelectorAll("input.js-price")).forEach(i => {
+    i.value = Math.round(i.value * 7.5);
+  });
+}
+
+function createUpdateInvoiceButton() {
+  let updateInvoiceButton = document.createElement("button");
+  updateInvoiceButton.onclick = updateInvoice;
+  updateInvoiceButton.innerText = "Update  hours to days";
+  updateInvoiceButton.className = "pds-button convert-hours-button";
+  document.body.appendChild(updateInvoiceButton);
+}
+
 window.onload = function () {
   if (window.location.pathname.indexOf("/time/week") !== -1) {
     createConvertToHoursButton();
   }
   if (!!window.location.pathname.match(projectEditPageRegex)) {
     createGetDailyRatesButton();
+  }
+  if (window.location.pathname.indexOf("/invoices/new_create") !== -1) {
+    createUpdateInvoiceButton();
   }
 }
 
