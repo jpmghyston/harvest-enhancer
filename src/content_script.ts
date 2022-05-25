725a99925d4a231bb76c3824fad2b7bea7639c73
Jake McKenna <Jake.McKenna@Ghyston.com>
2022-05-25 16:24:16 +0100

    Add days to project breakdown summary

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,5 +1,5 @@
 import { MutationSummary, Summary } from "mutation-summary";
-
+const hoursInADay = 7.5;
 const hoursToHourMinuteString = (hours: number): string => {
   const hour = Math.floor(hours);
   const minute = Math.round((hours - hour) * 60);
@@ -13,7 +13,7 @@ const convertToHours = function () {
     .map(elem => elem as HTMLInputElement)
     .filter(i => i.value.startsWith("0:") || i.value == "1:00")
     .forEach(i => {
-      i.value = hoursToHourMinuteString(i.value == "1:00" ? 7.5 : parseFloat(i.value.substring(2)) * 7.5 / 60);
+      i.value = hoursToHourMinuteString(i.value == "1:00" ? hoursInADay : parseFloat(i.value.substring(2)) * hoursInADay / 60);
       i.dispatchEvent(new Event("change"));
     });
   // @ts-ignore
@@ -21,8 +21,6 @@ const convertToHours = function () {
   // @ts-ignore
   setTimeout(() => document.querySelector('button.js-close-modal').click(), 300);
 }
-
-const projectEditPageRegex = /\/projects\/[0-9]+\/edit.*/;
 
 function createConvertToHoursButton() {
   let convertToHoursButton = document.createElement("button");
@@ -63,7 +61,7 @@ const addDailyRatesToPage = function() {
         return;
       }
       const hourlyRate = parseFloat((Array.from(billableRateElement!.children)[1] as HTMLInputElement).value);
-      const dailyRate = Math.round(hourlyRate * 7.5);
+      const dailyRate = Math.round(hourlyRate * hoursInADay);
       const dailyRateCell = document.createElement("td");
       dailyRateCell.className = "daily-rate-cell";
       dailyRateCell.innerText = dailyRate.toString();
@@ -89,6 +87,52 @@ const createUpdateDailyRatesObserver = function () {
   });
 }
 
+function addTrackedHoursToDays() {
+  // Add days header, if it doesn't already exist
+  const tableHeader = document.querySelector("table.project-analysis-table-breakdown tbody tr.tbody-head");
+  const daysHeader = document.createElement("th");
+  const daysHeaderClassName = "days-header";
+  daysHeader.className = daysHeaderClassName;
+  daysHeader.innerText = "Days";
+  if (!tableHeader?.querySelector(`th.${daysHeaderClassName}`)) {
+    tableHeader?.appendChild(daysHeader);
+  }
+
+  // Add days cells
+  const taskRows = Array.from(document.querySelectorAll("table.project-analysis-table-breakdown tbody tr")).filter(row => row.className.indexOf("tbody-head") === -1);
+  taskRows.forEach(row => {
+    const hoursElement = row.querySelector("td.total") as HTMLTableCellElement;
+    if (!hoursElement) {
+      return;
+    }
+    const hours = parseFloat(hoursElement.innerText);
+    const days = hours / hoursInADay;
+    const daysCell = document.createElement("td");
+    daysCell.className = "days-cell";
+    daysCell.innerText = days.toFixed(2);
+    addOrReplaceChildElementByClassName(row as HTMLElement, daysCell, "days-cell");
+  });
+}
+
+
+const createTrackedHoursToDaysObserver = function () {
+  const ms = new MutationSummary({
+    callback(summaries: Summary[]) {
+      summaries.forEach((summary: Summary) =>  {
+        // @ts-ignore
+        if (summary.added.filter(elem => elem).length > 0) {
+          addTrackedHoursToDays();
+        }
+        console.log(summary)
+      });
+    },
+    queries: [
+      { all: true }
+    ]
+  });
+}
+
+
 const updateInvoice = function () {
   const ROUNDING_TOLERANCE = 0.05;
   // @ts-ignore
@@ -97,27 +141,30 @@ const updateInvoice = function () {
 
     const inputElement = elem as HTMLInputElement;
     const inputValue = parseFloat(inputElement.value);
-    const newValue = inputValue / 7.5;
-    const newValueRounded = (Math.round((inputValue * 4) / 7.5) / 4).toFixed(2);
+    const newValue = inputValue / hoursInADay;
+    const newValueRounded = (Math.round((inputValue * 4) / hoursInADay) / 4).toFixed(2);
     if (Math.abs(newValue - parseFloat(newValueRounded)) > ROUNDING_TOLERANCE) {
       alert(`Couldn't round value ${inputValue} to a quarter-day accurately (${newValue} vs ${newValueRounded}), so leaving as-is`);
     } else {
-      inputElement.value = (Math.round((inputValue * 4) / 7.5) / 4).toFixed(2);
+      inputElement.value = (Math.round((inputValue * 4) / hoursInADay) / 4).toFixed(2);
     }
   });
   Array.from(document.querySelectorAll("input.js-price")).forEach(elem => {
     const inputElement = elem as HTMLInputElement;
-    inputElement.value = Math.round(parseFloat(inputElement.value) * 7.5).toFixed(2);
+    inputElement.value = Math.round(parseFloat(inputElement.value) * hoursInADay).toFixed(2);
   });
 }
 
 function createUpdateInvoiceButton() {
   let updateInvoiceButton = document.createElement("button");
   updateInvoiceButton.onclick = updateInvoice;
-  updateInvoiceButton.innerText = "Update  hours to days";
+  updateInvoiceButton.innerText = "Update hours to days";
   updateInvoiceButton.className = "pds-button convert-hours-button";
   document.body.appendChild(updateInvoiceButton);
 }
+
+const projectEditPageRegex = /\/projects\/[0-9]+\/edit.*/;
+const projectsPageRegex = /\/projects\/[0-9]+$/;
 
 window.onload = function () {
   if (window.location.pathname.indexOf("/time/week") !== -1) {
@@ -131,6 +178,9 @@ window.onload = function () {
   }
   if (window.location.pathname.indexOf("/invoices/new") !== -1) {
     createUpdateInvoiceButton();
+  }
+  if (!!window.location.pathname.match(projectsPageRegex)) {
+    createTrackedHoursToDaysObserver();
   }
 }
 
